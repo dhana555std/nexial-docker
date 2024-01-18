bdae30b41f065b8d3d8636dc928a32f78fa20c29
ACCIONLABS\al5170 <sundari.harika@accionlabs.com>
2024-01-18 14:43:40 +0530

    adding workflow file

---
+++ v1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Example 1
+curl --location --request POST 'https://api.boomi.com/mdm/api/rest/v1/trainingharikasundari-DADFVL/clouds/3229df3b-0682-44c4-b900-57b8a249c17b/repositories/property${environment}/create' \
+--header 'Authorization: Basic c3VuZGFyaS5oYXJpa2FAYWNjaW9ubGFicy5jb206Qm9vbWlQbGF0Zm9ybUAzMzM=' \
+
